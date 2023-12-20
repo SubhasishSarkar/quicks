600ef4084d9540e8f15f4072febc05fa26b58bd6
SubhasishSarkar <sarkarsubhasish207@gmail.com>
2023-12-20 20:08:30 +0530

    inital

--- src/layout/admin/Footer.jsx
+++ src/layout/admin/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <footer className="footer--pin">
-            <div className="d-grid d-md-flex justify-content-md-center">BM-SSY Management System © 2022-2023</div>
+            <div className="d-grid d-md-flex justify-content-md-center">QUICKS ADMIN PANNEL © 2022-2023</div>
         </footer>
     );
 };
